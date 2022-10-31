24438636b17baefdbf726e03756a88879918cc4f
zach wick <zach@zachwick.com>
2022-10-30 20:40:13 -0400

    headline change
    Signed-off-by: zach wick <zach@zachwick.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Learning Design Showcase',
+  title: 'zach wick',
   tagline: 'Making documentation experiences educational',
   url: 'https://showcase.zachwick.com',
   baseUrl: '/',
