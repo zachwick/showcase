c3865bdbf5aa90145bb0156a9a0c40154ed77dfa
zach wick <zach@zachwick.com>
2022-11-17 14:35:34 -0500

    WIP polish
    Signed-off-by: zach wick <zach@zachwick.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,59 +3,53 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: 'Approachable content',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Created using the same educational design principles that it speaks to and only assuming that consumers have a desire to learn, this content is accessible to a wide variety of learners.
-      </>
-    ),
-  },
-  {
-    title: 'Digital focus',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Focusing on digital educational experiences enables easy extrapolation to real-world developer enablement scenarios.
-      </>
-    ),
-  },
-  {
-    title: 'Useful today',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Presented as a field guide for doers, each artifact is intended to provide a standalone insight that can be applied by learners today.
-      </>
-    ),
-  },
+    {
+        title: 'instructional designer',
+        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+        description: (
+            <></>
+        ),
+    },
+    {
+        title: 'software engineer',
+        Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+        description: (
+            <></>
+        ),
+    },
+    {
+        title: 'local government legislator',
+        Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+        description: (
+            <></>
+        ),
+    },
 ];
 
 function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img"/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
