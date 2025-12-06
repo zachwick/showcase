a83d779675486947322473f1a7c347a257e1935d
zach wick <zach@btrbet.app>
2025-12-06 12:56:59 -0500

    Upgrades packages'
    Signed-off-by: zach wick <zach@btrbet.app>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,8 +11,13 @@ const config = {
   url: 'https://zachwick.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
